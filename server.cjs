feed3c7ef29e982c62734bf855cd5a9d2b122060
Cookie <alstrokash@gmail.com>
2024-05-06 22:52:18 +0300

    Ably working version

--- server.cjs
+++ server.cjs
@@ -3,16 +3,15 @@ const express = require("express");
 const socket = require("socket.io");
 const http = require("http");
 const { Server } = require("socket.io");
+const Ably = require("ably");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const port = 3001;
+const port = 3000;
 
 var history = [];
 var colors = ["rgba(73, 158, 255, 0.53)", "rgba(234, 73, 255, 0.53)"];
-
-//__dirname+="/../";
 
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
@@ -21,62 +20,137 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/greet", (req, res) => {
-  res.send({ msg: "hello world" });
+//Ably
+const key = "3JcURg.fOPmbg:efBqHXpjjaogZHqRcPvOOsPuiyJJt7y0UCTcQwI_GAA";
+const realtime = new Ably.Realtime(key);
+// console.log(realtime);
+var serverChannel = realtime.channels.get("server-ch");
+var clientChannel = realtime.channels.get("client-ch");
+clientChannel.publish("update", { time: Date.now() });
+//
+
+serverChannel.subscribe((message) => {
+  switch (message.name) {
+    case "connected":
+      if (message.data["name"] == "") {
+        giveNameNRestoreHistory();
+      } else clientChannel.publish("history", { history: history });
+      break;
+    case "sendMessage":
+      history.push(message);
+      if (history.length > 10) {
+        history.shift();
+      }
+      // console.log(message.data["text"]);
+      clientChannel.publish("newMessage", { text: message.data["text"] });
+      break;
+
+    default:
+      console.log(message);
+      break;
+  }
 });
 
-//io.on("connection", (socket) => {
-//  fetch("https://random-word-form.herokuapp.com/random/adjective")
-//    .then((response) => {
-//      if (!response.ok) {
-//        throw new Error("Network response was not ok");
-//      }
-//      return response.json();
-//    })
-//    .then((data1) => {
-//      fetch("https://random-word-form.herokuapp.com/random/noun")
-//        .then((response) => {
-//          if (!response.ok) {
-//            throw new Error("Network response was not ok");
-//          }
-//          return response.json();
-//        })
-//        .then((data2) => {
-//          console.log(data1[0] + " " + data2[0]);
-//          socket.emit("setClientName", data1[0] + " " + data2[0]);
-//        })
-//        .catch((error) => {
-//          console.error("Error:", error);
-//        });
-//    })
-//    .catch((error) => {
-//      console.error("Error:", error);
-//    });
+function giveNameNRestoreHistory() {
+  fetch("https://random-word-form.herokuapp.com/random/adjective")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data1) => {
+      fetch("https://random-word-form.herokuapp.com/random/noun")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data2) => {
+          console.log(data1[0] + " " + data2[0]);
+          clientChannel.publish("setClientName", {
+            name: data1[0] + " " + data2[0],
+          });
+          clientChannel.publish("history", { history: history });
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 
-//  console.log(`new user connected: ${socket.id}`);
+  console.log(`new user connected: ${data1[0] + " " + data2[0]}`);
+}
 
-//  socket.emit("history", history);
+/*
+async function doPubSub() {
+  await realtime.connection.once("connected");
+  console.log("Working");
 
-//  socket.on("sendMessage", (message) => {
-//    //console.log(message);
-//    history.push(message);
-//    if (history.length > 10) {
-//      history.shift();
-//    }
-//    io.emit("newMessage", message);
-//  });
+  const channel = realtime.channels.get("main-ch");
 
-//  socket.on("disconnect", () => {
-//    console.log("user disconnected");
-//  });
-//});
+  channel.subscribe((message) => {
+    console.table(message);
+  });
+  channel.publish("update", "Hello, Ably!");
+}
+doPubSub();*/
 
-//server.listen(port, () => {
-//  console.log(`Example app listening on port ${port}`);
-//});
+/* Old with IO
+io.on("connection", (socket) => {
+  fetch("https://random-word-form.herokuapp.com/random/adjective")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data1) => {
+      fetch("https://random-word-form.herokuapp.com/random/noun")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data2) => {
+          console.log(data1[0] + " " + data2[0]);
+          socket.emit("setClientName", data1[0] + " " + data2[0]);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  console.log(`new user connected: ${socket.id}`);
+
+  socket.emit("history", history);
+
+  socket.on("sendMessage", (message) => {
+    //console.log(message);
+    history.push(message);
+    if (history.length > 10) {
+      history.shift();
+    }
+    io.emit("newMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+*/
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 app.post("/send", (req, res) => {
   console.log(req.body);
 });
-
-module.exports = app;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import io from "socket.io-client";
+
 const messages = document.getElementById("messages");
 const textbox = document.getElementById("textbox");
 const sendButton = document.getElementById("sendButton");
@@ -16,6 +17,68 @@ async function createFetch(api, body) {
   });
 }
 
+// Ably
+const key = "3JcURg.fOPmbg:efBqHXpjjaogZHqRcPvOOsPuiyJJt7y0UCTcQwI_GAA";
+const realtime = new Ably.Realtime(key);
+var serverChannel = realtime.channels.get("server-ch");
+var clientChannel = realtime.channels.get("client-ch");
+
+serverChannel.publish("connected", { name: clientName });
+
+clientChannel.subscribe((message) => {
+  switch (message.name) {
+    case "newMessage":
+      addMessage(message.data["text"]);
+      break;
+
+    case "setClientName":
+      if (clientName == "") clientName = message.data["name"];
+      break;
+
+    case "history":
+      console.log(message.data["history"]);
+      for (let i = 0; i < message.data["history"].length; i++) {
+        addMessage(message.data["history"][i].data["text"]);
+      }
+      break;
+
+    default:
+      console.log(message);
+      break;
+  }
+});
+//
+
+sendButton.addEventListener("click", SendClick);
+
+textbox.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    SendClick();
+  }
+});
+
+async function SendClick() {
+  serverChannel.publish("sendMessage", {
+    text: clientName + ": " + textbox.value,
+  });
+  textbox.value = "";
+}
+
+function addMessage(messageText) {
+  const message = document.createElement("li"); // style='color: ${'rgba(73, 158, 255, 0.53)'}
+  message.innerHTML =
+    `<span class='highlight' '>` +
+    messageText.substring(0, messageText.indexOf(":")) +
+    "</span>" +
+    ": " +
+    messageText.substring(messageText.indexOf(":") + 1);
+  messages.appendChild(message);
+  if (messages.children.length > 10) {
+    messages.removeChild(messages.firstChild);
+  }
+}
+
+/* Old with IO
 //Send button & Enter
 sendButton.addEventListener("click", SendClick);
 
@@ -26,7 +89,7 @@ textbox.addEventListener("keyup", (event) => {
 });
 
 socket.on("setClientName", (name) => {
-  console.log("yes");
+  //console.log("yes");
   clientName = name;
 });
 
@@ -64,3 +127,4 @@ function addMessage(messageText) {
     messages.removeChild(messages.firstChild);
   }
 }
+*/
