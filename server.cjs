e07072f187a7d183c3531944ff0dc0e4f42c48f0
CookieOfWar <alstrokash@gmail.com>
2024-05-07 09:34:11 +0300

    table is the same log

--- server.cjs
+++ server.cjs
@@ -25,6 +25,7 @@ const key = "3JcURg.fOPmbg:efBqHXpjjaogZHqRcPvOOsPuiyJJt7y0UCTcQwI_GAA";
 const realtime = new Ably.Realtime(key);
 var serverChannel = realtime.channels.get("server-ch");
 var clientChannel = realtime.channels.get("client-ch");
+console.table(serverChannel, clientChannel);
 clientChannel.publish("update", { time: Date.now() });
 //
 
